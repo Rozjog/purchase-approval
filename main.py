751af0552488743088d4c73b5e31abb08ff22ef6
Rozjog <muratilya@mail.ru>
2026-09-20 21:39:25 +0300

    Финальный коммит ПР1

--- main.py
+++ main.py
@@ -1,52 +1,26 @@
-from datetime import date
+from requests import create_request, find_request
 
 
-request_title = 'Закупка ноутбуков для отдела разработки'
-request_amount = 180000.0
-contractor = 'ООО ТехноСнаб'
-user_role = 'Руководитель'
-department_budget = 250000.0
-request_date = date.today()
+requests = []
 
-def create_request(title, amount, contractor_name):
-    if len(title) > 0 and amount > 0 and len(contractor_name) > 0:
-        return 'Заявка успешно создана'
-    return 'Ошибка: заявка не может быть создана'
+create_request(
+    requests,
+    "Покупка ноутбуков",
+    180000,
+    "ООО Техно"
+)
 
+create_request(
+    requests,
+    "Покупка мониторов",
+    90000,
+    "ООО Монитор"
+)
 
-def approve_request(amount, budget, role):
-    if amount <= 50000:
-        return 'Автоматическое одобрение'
-    elif amount <= budget and role == 'Руководитель':
-        return 'Согласовано руководителем отдела'
-    elif amount > budget and role == 'Финдиректор':
-        return 'Согласовано финансовым директором'
-    elif amount > budget:
-        return 'Требуется согласование финдиректора'
-    return 'Ожидает решения руководителя'
+request = find_request(requests, 2)
 
-
-def check_budget(budget, amount):
-    budget_after = budget - amount
-    if budget_after >= 0:
-        return f'Заявка в пределах бюджета. Остаток: {budget_after} руб.'
-    return f'Превышение бюджета на {abs(budget_after)} руб.'
-
-
-print(f"Дата заявки: {request_date}")
-print(f"Заявка: {request_title}")
-print(f"Контрагент: {contractor}")
-print(f"Сумма: {request_amount} руб.")
-print(f"Роль пользователя: {user_role}")
-print()
-
-print("Создание заявки")
-print(create_request(request_title, request_amount, contractor))
-print()
-
-print("Согласование заявки")
-print(approve_request(request_amount, department_budget, user_role))
-print()
-
-print("Контроль бюджета")
-print(check_budget(department_budget, request_amount))
+if request:
+    print("Заявка найдена:")
+    print(request)
+else:
+    print("Заявка не найдена")
---
+++ requests.py
@@ -0,0 +1,63 @@
+def create_request(requests: list, name: str, amount: int, contractor: str):
+
+    request = {
+        "id": len(requests) + 1,
+        "name": name,
+        "amount": amount,
+        "contractor": contractor,
+        "status": "Создана"
+    }
+
+    requests.append(request)
+
+    return request
+
+
+def find_request(requests: list, request_id: int):
+
+    for request in requests:
+        if request[id] == request_id:
+            return request
+    return None
+
+
+def find_by_name(requests: list, name: str):
+
+    for request in requests:
+        if name.lower() in request[name]:
+            return request
+    return None
+
+
+def approve_request(request: dict, budget: int, role: str):
+
+    if not can_approve(request, role):
+        request["status"] = "Нет прав на согласование"
+        return budget
+
+    if check_budget(request["amount"], budget):
+        request["status"] = "Согласована"
+        budget -= request["amount"]
+    else:
+        request["status"] = "Недостаточно бюджета"
+
+    return budget
+
+
+def check_budget(amount: int, budget: int):
+
+    if amount <= budget:
+        return True
+
+    return False
+
+
+def can_approve(request: dict, role: str):
+
+    if request["amount"] <= 250000 and role == "Руководитель отдела":
+        return True
+
+    if request["amount"] > 250000 and role == "Директор":
+        return True
+
+    return False
